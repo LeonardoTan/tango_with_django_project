5d24e87a5438f7fbf154b67aff06678c033f5d0e
Your Name <you@example.com>
2016-06-27 18:49:36 +0800

    work to capter 7

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from rango.models import Category, Page
 
 
 def index(request):
-    context_dict = {"boldmessage": "I am bold font from the context."}
+    category_list = Category.objects.order_by('-likes')[:5]
+    pages = Page.objects.order_by('-views')[:5]
+    context_dict = {"category_list": category_list, "pages": pages}
     return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
     return render(request, 'rango/about.html')
+
+
+def category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoseNotExist:
+        pass
+    return render(request, 'rango/category.html', context_dict)
